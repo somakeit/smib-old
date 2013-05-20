9680facad40638f41f821bcf6c0f67077a43afd9
Bracken Dawson <abdawson@gmail.com>
2013-05-21 00:59:03 +0100

    Secure prototype that runs commands
    only when speaking to the channel, /msg context to be implimented.

--- smib.pl
+++ smib.pl
@@ -4,12 +4,14 @@ use strict;
 use warnings;
 
 use POE qw(Component::IRC);
+use IPC::System::Simple qw(capture);
+use String::Escape qw(printable);
 
 my $nickname = 'smibtest';
 my $ircname  = 'So Make It Bot';
-my $programsdir = '/home/alan/irccat-commands/';
+my $programsdir = '/home/xbmc/irccat-commands/';
 my $server   = 'holmes.freenode.net';
-my @channels = ('#botwar', '#southackton');
+my @channels = ('#smibtest');
 
 #create a new PoCo-IRC object
 my $irc = POE::Component::IRC->spawn(nick    => $nickname,
@@ -48,27 +50,35 @@ sub irc_001 {
   return;
 }
 
-# 
+# like when someone says somthing in a channel
 sub irc_public {
   my ($sender, $who, $where, $what) = @_[SENDER, ARG0 .. ARG2];
   my $nick = ( split /!/, $who )[0];
   my $channel = $where->[0];
 
   #this launches commands in public context
-  #damn it benjie I told you file extensions were daft
-
   my @output;
   if ($what =~ m/\?(\w+) {0,1}(.*)/) {
-    @output = system("$programsdir$1", "$who", "$where", "$where", "$2");
+    #damn it Benjie I told you file extensions were daft
+    my $command = $1;
+    my @commands = capture('find', "$programsdir", '-type', 'f', '-name', "$command\.*");
+    if (@commands < 1) {
+      $irc->yield( privmsg => $channel => "Sorry $nick, I don't have a $command command." );
+      return;
+    }
+    $command = shift @commands;
+    my $argline = printable($2);
+    chomp $command;
+    chdir $programsdir;
+    @output = capture("$command", "$nick", "$channel", "$channel", "$argline"); #capture does not invoke a shell if it has more than one argument
   }
-  
   for my $line (@output) {
-    $irc->yield( privmsg => $channel => "$line" );
+    $irc->yield( privmsg => $channel => $line );
   }
   return;
 }
 
-# We registered for all events, this will produce some debug info.
+# This will catch everything we don't and offer clues about what to catch
 sub _default {
    my ($event, $args) = @_[ARG0 .. $#_];
    my @output = ( "$event: " );
